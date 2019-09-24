a795851af02a3d276255c8e773c383d26bd6a612
Robert Wiggins <wigginsro11@gmail.com>
2019-09-23 22:14:34 -0400

    converted requests to render templates

--- src/pages/views.py
+++ src/pages/views.py
@@ -3,13 +3,14 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_view(request, *args, **kwargs): # *args, **kwargs
-    return HttpResponse("<h1>Hello World</h1>") # string of HTML code
+    # return HttpResponse("<h1>Hello World</h1>") # string of HTML code
+    return render(request, "home.html", {})
 
 def contact_view(request, *args, **kwargs):
-    return HttpResponse("<h1>Contact</h1>")
+    return render(request, "contact.html", {})
 
 def about_view(request, *args, **kwargs):
-    return HttpResponse("<h1>About</h1>")
+    return render(request, "about.html", {})
 
 def social_view(request, *args, **kwargs):
-    return HttpResponse("<h1>Social</h1>")
+    return render(request, "social.html", {})
